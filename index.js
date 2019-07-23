ccd8f49fa031142d6ac77b7ff69832b99ee52785
Naila <nailakaliyeva@gmail.com>
2019-07-23 21:27:51 +0000

    created an array of random objects

--- index.js
+++ index.js
@@ -8,70 +8,79 @@ for(i=0;i<classIcons.length; i++){
     classIcons[i].innerHTML = cards.suits[randomCard];
 }
 
-let classValue = document.querySelectorAll(".cardValue");
-let randomValue = Math.floor(Math.random()*13)
-for(i=0;i<classValue.length; i++){
-    classValue[i].innerHTML = cards.value[randomValue];
-}
+// let classValue = document.querySelectorAll(".cardValue");
+// let randomValue = Math.floor(Math.random()*13)
+// for(i=0;i<classValue.length; i++){
+//     classValue[i].innerHTML = cards.value[randomValue];
+// }
+window.onload = function(){
+function createRandomArray(inputValue){
+    let randomArray = [];
 
-function draw(){
-    let initialArray = new Array(input.value);
-    for (i=0; i<=initialArray.length; i++){
-        return (
-            initialArray.push(cards.value[randomValue])
-        );
-        document.querySelector(".cardsInline").innerHTML = <div class="card">
-                                                            <div class="top">
-                                                                <span class="icons"><i class="fas fa-heart"></i></span>
-                                                                <span class="cardValue"><h4>9</h4></span>
-                                                            </div>
+    for(i=0;i<inputValue; i++){
+        let randomCard = Math.floor(Math.random()*4);
+        let randomValue = Math.floor(Math.random()*13);
+        randomArray[i] = {suit: cards.suits[randomCard], value: cards.value[randomValue]};
 
-                                                            <div class="bottom">
-                                                                <span class="icons"><i class="fas fa-heart"></i></span>
-                                                                <span class="cardValue"><h4>9</h4></span>
-                                                            </div>
-                                                    </div>
-}
     }
+return randomArray;
 
+};
+console.log(createRandomArray(5));
+}
+// function draw(inputValue){
+//     // let initialArray = new Array(input.value);
+//     for (i=0; i<=; i++){
+//         return (
+//             initialArray.push(cards.value[randomValue])
+//         );
+//         document.querySelector(".cardsInline").innerHTML = <div class="card">
+//                                                             <div class="top">
+//                                                                 <span class="icons"><i class="fas fa-heart"></i></span>
+//                                                                 <span class="cardValue"><h4>9</h4></span>
+//                                                             </div>
 
-
-
-
-// switch(cards.value[randomValue]){
-// case "J":
-//         document.querySelector(".card").style.backgroundImage = 'url("https://cdn4.iconfinder.com/data/icons/playing-card-thinline/32/ico-jack-of-clubs-512.png")';
-//         break;
-//     case "Q": document.querySelector(".card").style.backgroundImage = 'url("https://image.shutterstock.com/image-vector/beautiful-face-queen-icon-600w-315462977.jpg")';
-//         break;
-//     case "K": document.querySelector(".card").style.backgroundImage = 'url("https://cdn3.iconfinder.com/data/icons/casino-and-gambling-icons/531/King-2-512.png")';
-//         break;
-//     case "A": document.querySelector(".card").style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/4/40/Brush-calligraphy-alphabet-a.jpg")';
-//         break;
-//     case "2": document.querySelector(".card").style.backgroundImage = 'url("https://martialartsworldnews.com/wp-content/uploads/2015/11/2.jpg")';
-//         break;
-//     case "3": document.querySelector(".card").style.backgroundImage = 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVcs4U8rsXVELmOVYk7pk735EaJ4AEQ33p0gQW2Pq64VcgUm8e1w")';
-//         break;
-//     case "4": document.querySelector(".card").style.backgroundImage = 'url("https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001")';
-//         break;
-//     case "5": document.querySelector(".card").style.backgroundImage = 'url("https://vignette4.wikia.nocookie.net/phobia/images/0/03/5.jpg/revision/latest?cb=20170102100251")';
-//         break;
-//     case "6": document.querySelector(".card").style.backgroundImage = 'url("https://blognumbers.files.wordpress.com/2010/09/6.jpg")';
-//         break;
-//     case "7": document.querySelector(".card").style.backgroundImage = 'url("https://vignette.wikia.nocookie.net/phobia/images/f/fe/7.jpg/revision/latest?cb=20170121103340")';
-//         break;
-//     case "8": document.querySelector(".card").style.backgroundImage = 'url("https://eightinc.com/wp-content/uploads/eight-logo-big.png")';
-//         break;
-//     case "9": document.querySelector(".card").style.backgroundImage = 'url("https://blognumbers.files.wordpress.com/2010/09/9.jpg")';
-//         break;
-//     case "10": document.querySelector(".card").style.backgroundImage = 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxI8CqOXB9u3MctnqFpE8qLpqHK2loTGLBAx958xBiUOkRR9KY_w")';
-//         break;
+//                                                             <div class="bottom">
+//                                                                 <span class="icons"><i class="fas fa-heart"></i></span>
+//                                                                 <span class="cardValue"><h4>9</h4></span>
+//                                                             </div>
+//                                                     </div>
+// }
 //     }
 
 
 
 
 
+// // switch(cards.value[randomValue]){
+// // case "J":
+// //         document.querySelector(".card").style.backgroundImage = 'url("https://cdn4.iconfinder.com/data/icons/playing-card-thinline/32/ico-jack-of-clubs-512.png")';
+// //         break;
+// //     case "Q": document.querySelector(".card").style.backgroundImage = 'url("https://image.shutterstock.com/image-vector/beautiful-face-queen-icon-600w-315462977.jpg")';
+// //         break;
+// //     case "K": document.querySelector(".card").style.backgroundImage = 'url("https://cdn3.iconfinder.com/data/icons/casino-and-gambling-icons/531/King-2-512.png")';
+// //         break;
+// //     case "A": document.querySelector(".card").style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/4/40/Brush-calligraphy-alphabet-a.jpg")';
+// //         break;
+// //     case "2": document.querySelector(".card").style.backgroundImage = 'url("https://martialartsworldnews.com/wp-content/uploads/2015/11/2.jpg")';
+// //         break;
+// //     case "3": document.querySelector(".card").style.backgroundImage = 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVcs4U8rsXVELmOVYk7pk735EaJ4AEQ33p0gQW2Pq64VcgUm8e1w")';
+// //         break;
+// //     case "4": document.querySelector(".card").style.backgroundImage = 'url("https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001")';
+// //         break;
+// //     case "5": document.querySelector(".card").style.backgroundImage = 'url("https://vignette4.wikia.nocookie.net/phobia/images/0/03/5.jpg/revision/latest?cb=20170102100251")';
+// //         break;
+// //     case "6": document.querySelector(".card").style.backgroundImage = 'url("https://blognumbers.files.wordpress.com/2010/09/6.jpg")';
+// //         break;
+// //     case "7": document.querySelector(".card").style.backgroundImage = 'url("https://vignette.wikia.nocookie.net/phobia/images/f/fe/7.jpg/revision/latest?cb=20170121103340")';
+// //         break;
+// //     case "8": document.querySelector(".card").style.backgroundImage = 'url("https://eightinc.com/wp-content/uploads/eight-logo-big.png")';
+// //         break;
+// //     case "9": document.querySelector(".card").style.backgroundImage = 'url("https://blognumbers.files.wordpress.com/2010/09/9.jpg")';
+// //         break;
+// //     case "10": document.querySelector(".card").style.backgroundImage = 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxI8CqOXB9u3MctnqFpE8qLpqHK2loTGLBAx958xBiUOkRR9KY_w")';
+// //         break;
+// //     }
 
 
 
@@ -88,33 +97,38 @@ function draw(){
 
 
 
-// let cards = {
-//     suits: ['<i class="fab fa-envira"></i>', '<i class="fab fa-canadian-maple-leaf"></i>', '<i class="fas fa-gem"></i>', '<i class="fas fa-heart"></i>'],
-//     value:['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
-// }
-// // let classIcons = document.querySelectorAll(".icons");
-// // let randomCard = Math.floor(Math.random()*4)
-// for(i=0;i<classIcons.length; i++){
-//     classIcons[i].innerHTML = cards.suits[randomCard];
-// }
 
-// let classValue = document.querySelectorAll("h1");
-// let randomValue = Math.floor(Math.random()*13)
-// for(i=0;i<classValue.length; i++){
-//     classValue[i].innerHTML = cards.value[randomValue];
-// }
-////
 
-// const cardHTML = (value, suit) => {
-//     return `
-// <div class="container">
-//     <div class="row">
-//         <!--THIS IS THE CARD-->
-//         <div class="col-1 offset-1 p-2 smallCard">
-//             <!--THIS IS THE TOP OF THE CARD-->
-//             <div class="row">
-//                 <!--THIS IS THE NORMAL ICONS-->
-//                 <div class="icons col-1 text-dark"><i class="fas fa-${suit}"></i></div>
+
+
+
+// // let cards = {
+// //     suits: ['<i class="fab fa-envira"></i>', '<i class="fab fa-canadian-maple-leaf"></i>', '<i class="fas fa-gem"></i>', '<i class="fas fa-heart"></i>'],
+// //     value:['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+// // }
+// // // let classIcons = document.querySelectorAll(".icons");
+// // // let randomCard = Math.floor(Math.random()*4)
+// // for(i=0;i<classIcons.length; i++){
+// //     classIcons[i].innerHTML = cards.suits[randomCard];
+// // }
+
+// // let classValue = document.querySelectorAll("h1");
+// // let randomValue = Math.floor(Math.random()*13)
+// // for(i=0;i<classValue.length; i++){
+// //     classValue[i].innerHTML = cards.value[randomValue];
+// // }
+// ////
+
+// // const cardHTML = (value, suit) => {
+// //     return `
+// // <div class="container">
+// //     <div class="row">
+// //         <!--THIS IS THE CARD-->
+// //         <div class="col-1 offset-1 p-2 smallCard">
+// //             <!--THIS IS THE TOP OF THE CARD-->
+// //             <div class="row">
+// //                 <!--THIS IS THE NORMAL ICONS-->
+// //                 <div class="icons col-1 text-dark"><i class="fas fa-${suit}"></i></div>
 
 //                 <div class="value col-1 offset-1"></div>
 
